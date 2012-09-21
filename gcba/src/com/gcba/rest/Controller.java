1ed49c0209c4120a28c1ccc4e00bd3f27f6d3e9f
matiasbargas <matiasbargas@gmail.com>
2012-09-21 17:06:37 -0300

    adding doGET Json response to servlet

--- gcba/src/com/gcba/rest/Controller.java
+++ gcba/src/com/gcba/rest/Controller.java
@@ -1,10 +1,16 @@
 package com.gcba.rest;
 
+import com.gcba.db.BolicheManager;
+import com.gcba.model.Boliche;
+import net.sf.json.util.JSONBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 
 public class Controller extends HttpServlet {
@@ -26,6 +32,13 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);    //To change body of overridden methods use File | Settings | File Templates.
+        PrintWriter writer = resp.getWriter();
+
+        List<Boliche> honduras = BolicheManager.getInstance().getBolichesByName("the");
+
+        for (Boliche hondura : honduras) {
+            writer.write(hondura.toJson().toString());
+        }
+
     }
 }
